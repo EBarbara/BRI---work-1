232b270280745626fdea6ba6babb72b2498f04ea
Estevan Barbará <estevan.barbara@gmail.com>
2017-06-25 09:39:49 -0300

    Lendo nomes dos arquivos do arquivo de configurações

--- QueryProcessor.py
+++ QueryProcessor.py
@@ -7,12 +7,23 @@ Created on Sun Jun 25 08:16:19 2017
 
 from lxml import etree
 
-parser = etree.XMLParser(dtd_validation=True)
-filequery = etree.parse("cfquery.xml", parser)
+def load_config(config_file):
+    with open(config_file) as config:
+        global input_file, query_file, result_file
+        input_file = config.readline().replace('LEIA=', '').rstrip()
+        query_file = config.readline().replace('CONSULTAS=', '').rstrip()
+        result_file = config.readline().replace('ESPERADOS=', '').rstrip()
+        
+def read_queries(input_file):
+    parser = etree.XMLParser(dtd_validation=True)
+    filequery = etree.parse(input_file, parser)
+    
+    for query in filequery.getroot().iterchildren("*"):
+        for element in query.iterchildren("*"):
+            print(element.tag, element.text)
+            for subelement in element.iterchildren("*"):
+                print(subelement.tag, subelement.attrib, subelement.text)
+        print("-------------------------------")
 
-for query in filequery.getroot().iterchildren("*"):
-    for element in query.iterchildren("*"):
-        print(element.tag, element.text)
-        for subelement in element.iterchildren("*"):
-            print(subelement.tag, subelement.attrib, subelement.text)
-    print("-------------------------------")
+load_config('pc.cfg')
+read_queries(input_file)
