eca6d58704ba2bb19e0202bffbd4d1ef48a5a87e
Estevan Barbará <estevan.barbara@gmail.com>
2017-06-30 18:47:16 -0300

    Módulo de Geração de Lista Invertida completamente funcional

--- App.py
+++ App.py
@@ -1,34 +1,36 @@
-import logging
-
-import time
-
-
-def start_logger():
-    logger = logging.getLogger("BRI - Exercise 1")
-    handler = logging.FileHandler('logs/BRI_1.log')
-    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    logger.setLevel(logging.INFO)
-    return logger
+import os
+import static
+from collections import defaultdict
+from InvertedListGenerator import InvertedListGenerator
 
 
 class App(object):
     def __init__(self):
-        self.logger = start_logger()
+        self.logger = static.start_logger()
 
-    def log_execution_time(self, title, start_time):
-        finish_time = time.time()
-        self.logger.info("Finish %s: %fs" % (title, finish_time - start_time))
+    def read_configuration_file(self, file):
+        start_time = static.get_current_time()
+        filename = os.path.basename(file)
+        self.logger.info('Reading Configuration File %s' % filename)
+        config = defaultdict(list)
+        with open(file) as config_file:
+            for line in config_file:
+                key = line.rstrip().split('=')[0]
+                value = line.rstrip().split('=')[1]
+                config[key].append(value)
+        static.log_execution_time('Reading Configuration File %s' % filename, self.logger, start_time)
+        return config
 
     def generate_inverted_index(self):
-        start_time = time.time()
-        self.log_execution_time('Inverted List Generate Module', start_time)
+        config = self.read_configuration_file('config/gli.cfg')
+        inverted_list_generator = InvertedListGenerator(config, self.logger)
+        inverted_list_generator.execute()
 
     def execute(self):
-        self.logger.info('Starting Workflow - BRI Exercise 1')
+        start_time = static.get_current_time()
+        self.logger.info('Starting BRI Exercise 1')
         self.generate_inverted_index()
-        pass
+        static.log_execution_time('BRI Exercise 1', self.logger, start_time)
 
 
 app = App()
--- InvertedListGenerator.py
+++ InvertedListGenerator.py
@@ -1,35 +1,77 @@
+import csv
+import os
+import static
+from collections import defaultdict
 from lxml import etree
+from nltk import word_tokenize
 
 
-def load_config(config_file):
-    global input_files, list_file
-    input_files = []
-    with open(config_file) as config:
-        for line in config:
-            operation = line.rstrip().split('=')[0]
-            file = line.rstrip().split('=')[1]
-            if operation == 'LEIA':
-                input_files.append(file)
-            elif operation == 'ESCREVA':
-                list_file = file
+class InvertedListGenerator(object):
+    def __init__(self, config, logger):
+        super(InvertedListGenerator, self).__init__()
+        self.logger = logger
+        self.input = config.get('LEIA')
+        self.output = config.get('ESCREVA')
+        self.documents = dict()
+        self.list = defaultdict(list)
 
+    def read_documents(self):
+        full_count = 0
+        start_time = static.get_current_time()
+        self.logger.info('Reading document abstracts')
+        parser = etree.XMLParser(dtd_validation=True)
+        for input_file in self.input:
+            count = 0
+            filename = os.path.basename(input_file)
+            doc_time = static.get_current_time()
+            self.logger.info('Reading Document File {0}'.format(filename))
+            file = etree.parse(input_file, parser)
+            for record in file.getroot().iterchildren():
+                key = 'invalid'
+                value = 'invalid'
+                for element in record.iterchildren():
+                    if element.tag == 'RECORDNUM':
+                        key = int(element.text)
+                    if element.tag == 'ABSTRACT' or element.tag == 'EXTRACT':
+                        value = element.text
+                if key != 'invalid':
+                    if value != 'invalid':
+                        self.documents[key] = word_tokenize(value)
+                        count += 1
+                    else:
+                        self.logger.warn('Document {0} has no info'.format(key))
+            static.log_execution_time('Reading Document File {0}'.format(filename), self.logger, doc_time)
+            self.logger.info("Read {0} documents from {1}".format(count, filename))
+            full_count += count
+        static.log_execution_time('Reading document abstracts', self.logger, start_time)
+        self.logger.info("Read {0} documents in total ".format(full_count))
 
-def read_data(input_file):
-    parser = etree.XMLParser(dtd_validation=True)
-    file = etree.parse(input_file, parser)
+    def generate_list(self):
+        start_time = static.get_current_time()
+        self.logger.info('Generating inverted list')
 
-    for record in file.getroot().iterchildren():
-        for element in record.iterchildren():
-            if element.tag == 'RECORDNUM':
-                print(element.tag, element.text)
-            elif element.tag == 'ABSTRACT':
-                print(element.tag, element.text)
-            elif element.tag == 'EXTRACT':
-                print(element.tag, element.text)
-        print("-------------------------------")
-    return file
+        for index, value in self.documents.items():
+            for word in value:
+                self.list[word].append(index)
+        static.log_execution_time('Generating inverted list', self.logger, start_time)
 
+    def write_list(self):
+        start_time = static.get_current_time()
+        output_file = self.output[0]
+        filename = os.path.basename(output_file)
+        self.logger.info('Writing Output File {0}'.format(filename))
+        with open(output_file, 'w+') as csvfile:
+            field_names = ['word', 'documents']
+            writer = csv.DictWriter(csvfile, delimiter=';', lineterminator='\n', fieldnames=field_names)
+            writer.writeheader()
+            for word in self.list:
+                writer.writerow({'word': word, 'documents': self.list[word]})
+        static.log_execution_time('Reading Output File {0}'.format(filename), self.logger, start_time)
 
-load_config('config/gli.cfg')
-for input_file in input_files:
-    read_data(input_file)
+    def execute(self):
+        start_time = static.get_current_time()
+        self.logger.info('Starting Inverted List Generate Module')
+        self.read_documents()
+        self.generate_list()
+        self.write_list()
+        static.log_execution_time('Inverted List Generate Module', self.logger, start_time)
