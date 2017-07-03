fb476c2084f0324d525f6703db18de4922be0c98
Estevan Barbará <estevan.barbara@gmail.com>
2017-07-03 17:30:04 -0300

    Introduzindo normalização na frequencia das palavars por documento

--- Indexer.py
+++ Indexer.py
@@ -1,11 +1,18 @@
 import ast
 import csv
 import os
-from collections import defaultdict
-
+from collections import defaultdict, Counter
 from math import log
 
 import static
+
+
+def calculate_max_frequency(document, word_frequency):
+    max_frequency = 0
+    for _, frequency in word_frequency.items():
+        if document in frequency.keys():
+            max_frequency = max(max_frequency, frequency[document])
+    return max_frequency
 
 
 class Indexer(object):
@@ -16,12 +23,14 @@ class Indexer(object):
         self.output_file = config.get('ESCREVA')[0]
         self.inverted_list = defaultdict(list)
         self.document_list = []
+        self.document_max = {}
         self.model = {}
 
     def read_inverted_list(self):
         start_time = static.get_current_time()
         filename = os.path.basename(self.input_file)
         self.logger.info('Reading inverted list from file {0}'.format(filename))
+        word_frequency = {}
         with open(self.input_file) as csv_file:
             field_names = ['word', 'documents']
             reader = csv.DictReader(csv_file, delimiter=';', lineterminator='\n', fieldnames=field_names)
@@ -29,11 +38,15 @@ class Indexer(object):
             for line in reader:
                 # Eliminando as palavras irrelevantes da lista invertida (palavras
                 # com menos de duas letras ou contendo algo diferente de letras)
-                if len(line['word']) > 2 and line['word'].isalpha():
+                word = line['word']
+                if len(word) > 2 and word.isalpha():
                     line_documents = ast.literal_eval(line['documents'])
-                    self.inverted_list[line['word']] = line_documents
+                    self.inverted_list[word] = line_documents
                     self.document_list.extend(line_documents)
+                    word_frequency.update({word: Counter(line_documents)})
         self.document_list = list(set(self.document_list))
+        for document in self.document_list:
+            self.document_max[document] = calculate_max_frequency(document, word_frequency)
         static.log_execution_time('Reading inverted list from file {0}'.format(filename), self.logger, start_time)
 
     def build_model(self):
@@ -45,8 +58,8 @@ class Indexer(object):
             idf = self.calculate_idf(documents_occurred)
             document_data = {}
             for document in documents_occurred:
-                tf = self.calculate_tf(word, document)
-                weight = tf * idf
+                tf_n = self.normalize_tf(word, document)
+                weight = tf_n * idf
                 document_data[document] = weight
             self.model[word] = (idf, document_data)
         static.log_execution_time('Generating vectorial model', self.logger, start_time)
@@ -54,8 +67,9 @@ class Indexer(object):
     def calculate_idf(self, documents_occurred):
         return log(len(self.document_list) / len(documents_occurred))
 
-    def calculate_tf(self, word, document):
-        return self.inverted_list[word].count(document)
+    def normalize_tf(self, word, document):
+        tf = self.inverted_list[word].count(document)
+        return tf/self.document_max[document]
 
     def write_model(self):
         start_time = static.get_current_time()
